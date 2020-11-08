44c4787b7f27930c122775e845cffd31ca33db8c
nam12 <namil0721@gmail.com>
2020-11-08 19:23:19 +0900

    1759

---
+++ dddd/1759.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+L, C = map(int, input().split())
+charList = [char for char in input().split()]
+charList.sort()
+
+def detector(x:list):
+    moCounter = 0
+    for i in x:
+        if i in ['a', 'e', 'i', 'o', 'u']:
+            moCounter += 1
+        else:
+            pass
+    if len(x) - moCounter < 2 or moCounter < 1:
+        return False
+    else:
+        return True
+
+res = combinations(charList, L)
+
+for i in res:
+    if detector(list(i)):
+        print(''.join(i))
